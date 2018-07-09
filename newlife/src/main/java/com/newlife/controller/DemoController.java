c09debe63c25742b0a6c8a26cc5437cb91e86ff4
wangyingman <553994825@qq.com>
2018-07-09 09:38:34 +0800

    first

---
+++ newlife/src/main/java/com/newlife/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.newlife.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "demo";
+    }
+}
